8ae862278fac37c21e4f39aabe98476d98213753
leohalldevall <leo.halldevall@elev.ga.ntig.se>
2023-09-19 09:17:09 +0200

    py

---
+++ module2.py
@@ -0,0 +1,21 @@
+#hello
+
+
+print("addition:",5+10)#addition
+print("multiplikation:",5*4)#Multiplikation
+print("diviision:",20/10)#Division
+print("potenser:",69 ** 5)
+namn=input ("name ")
+print ("hello" + namn + "how old are you")
+ålder = int(input("hur gammal är du")) 
+
+
+tal1=int(input ("tal1 "))
+tal2=int(input ("tal2 "))
+print (tal1 * tal2)
+
+längd=int(input ("längd i cm "))
+vikt=int(input ("vikt i kg "))
+längd= längd/100
+print (vikt / (längd ** 2))
+
